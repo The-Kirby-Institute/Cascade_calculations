1393a462ca0618a4fbe2110beaaf2cc12626b413
leftygray <leftygray@gmail.com>
2018-05-07 15:53:39 +1000

    Updated chlamydia cascade functions and results

--- code/PlotOptions.R
+++ code/PlotOptions.R
@@ -1,45 +1,49 @@
-## PlotOptions.R
+#' A function to specify ggplot options
+#'
+#' This function stores all my favoured plotting options and themes for
+#' ggplot plots.
+#'
+#' @param opt Name of option to use in plot.
+#' @return ggplot theme options object.
+#'
+#' @author Richard T. Gray, \email{Rgray@kirby.unsw.edu.au}
+#'
+#' @examples
+#' PlotOptions()
+#'
+#' @export
+#' @import ggplot2
+#'
+PlotOptions <- function(opt=c("main")) {
 
-# This script stores all my favoured plotting options and colours
-# It just needs to be sourced in a script for use. 
 
-# R. T. Gray 
+  opt <- match.arg(opt)
 
-# Default plot specifications
-graphics.off()
-# Load ggplot2
-require(ggplot2)
+  # Baseline theme for plot variables
+  main <- theme_bw() + theme(text = element_text(face = "bold",size=12,colour="black"),
+    axis.text.x = element_text(face = "plain",size=10,colour="black"),
+    axis.text.y = element_text(face = "plain",size=10,colour="black"),
+    axis.line.x = element_line(colour = "black"),
+    axis.line.y = element_line(colour = "black"),
+    axis.ticks = element_line(colour="black"),
+    legend.position = "top",
+    legend.background = element_rect(),
+    legend.key = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(colour = "black"),
+    axis.line = element_line(colour = "black"),
+    plot.title=element_text(size=12, face="bold"),
+    strip.background = element_blank()
+  )
 
-# Baseline theme for plot variables
-plotOpts <- theme_bw() + theme(text = element_text(face = "bold", 
-                                  size = 12, colour = "black"),
-                               axis.text.x = element_text(face = "plain",
-                                  size = 10, colour = "black"),
-                               axis.text.y = element_text(face = "plain",
-                                  size = 10, colour = "black"),
-                               axis.line.x = element_line(colour = "black"),
-                               axis.line.y = element_line(colour = "black"),
-                               axis.ticks = element_line(colour = "black"),
-                               legend.position = "top",
-                               legend.background = element_rect(),
-                               legend.key = element_blank(),
-                               panel.grid.major = element_blank(), 
-                               panel.grid.minor = element_blank(), 
-                               panel.background = element_blank(), 
-                               panel.border = element_rect(colour = "black"),
-                               axis.line = element_line(colour = "black"),
-                               plot.title=element_text(size = 12, 
-                                                       face="bold"),
-                               strip.background = element_blank()
-)
+  # Return theme
+  plotOpts <- switch(match.arg(opt),
+    main=main)
 
-# Setup colours
-# Palette with black and grey (colour blind friendly)
-cbPalette <- c("#000000","#999999", "#E69F00", "#56B4E9", "#009E73", 
-               "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  return(plotOpts)
 
-# maincols <- brewer.pal(4,"Set1")
+}
 
-# Colour scheme used in ASR
-asrcols <- c("#621C20", "#AB2322", "#A63603", "#D95728", "#E97164")
 
