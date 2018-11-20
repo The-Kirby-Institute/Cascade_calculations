9beeb0630fed344207ae35b1d1ccd76895cf92ac
leftygray <leftygray@gmail.com>
2018-11-21 10:53:21 +1100

    Created function for adjusting and producing annual diagnoses

---
+++ HIV/code/AnnualDiagnoses.R
@@ -0,0 +1,145 @@
+#' Calculate the annual number of notifications/diagnoses
+#' 
+#' This function calculates the number of annual notifications each year for
+#' a specific set of criteria. Adjusting to account for missing 
+#' information. 
+#' 
+#' @param hivSet Set of notifications known to meet the criteria and are 
+#' included in the analysis. 
+#' @param hivSetExcluded Set of notifications known to not meet the 
+#' criteria and are excluded from the analysis. 
+#' @param hivSetUnknown 
+#' @param allYears Vector specifying the years for calculations. 
+#' @param doAge Logical specifying if proportion males is calculated for 
+#' each age group.
+#' 
+#' @return A list containing up to two data frames. 
+#' 
+#' @author Richard T Gray, \email{Rgray@kirby.unsw.edu.au}
+#' 
+#' @export
+#' @import tidyverse
+#'  
+AnnualDiagnoses <- function(hivSet, hivSetExcluded, hivSetUnknown, 
+  allYears, doAge) {
+  
+  # analysisYear <- tail(allYears, 1)
+  
+  includeDiags <- hivSet %>% 
+    group_by(yeardiagnosis) %>%
+    summarise(included = n()) %>%
+    ungroup() %>%
+    rename(year = yeardiagnosis)
+  includeDiags <- FillDataFrame(allYears, includeDiags)
+  
+  # If excluded and unknown is empty replace with zeros
+  if (nrow(hivSetExcluded) == 0) {
+    excludedDiags <- data_frame(year = allYears,
+      excluded = 0)
+  } else {
+    excludedDiags <- hivSetExcluded %>%
+      group_by(yeardiagnosis) %>% 
+      summarise(excluded = n()) %>%
+      rename(year = yeardiagnosis)
+    
+  }
+  excludedDiags <- FillDataFrame(allYears, excludedDiags)
+  
+  if (nrow(hivSetUnknown) == 0) {
+    unknownDiags <- data_frame(year = allYears,
+      unknown = 0)
+  } else {
+    unknownDiags <- hivSetUnknown %>%
+      group_by(yeardiagnosis) %>% 
+      summarise(unknown = n()) %>%
+      rename(year = yeardiagnosis)
+  }
+  unknownDiags <- FillDataFrame(allYears, unknownDiags)
+  
+  # Adjust included and excluded annual diagnoses propotionally to replace
+  # unknowns
+  
+  ##*********************************************************************##
+  ## WARNING -- The following calculation to adjust included notifications 
+  ## can be incorrect if multiple selections are used which include 
+  ## selections with lots of missing data unless appropriate adjustments are
+  ## made. 
+  ##*********************************************************************##
+  
+  adjustDiags <- includeDiags %>% 
+    left_join(excludedDiags, by = "year") %>%
+    left_join(unknownDiags, by = "year") %>%
+    mutate(all = included + excluded + unknown) %>%
+    mutate(included = ifelse(is.na(included), 0, included),
+      excluded = ifelse(is.na(excluded), 0, excluded),
+      unknown = ifelse(is.na(unknown), 0, unknown)) %>%
+    mutate(prop_included = included / (included + excluded),
+      prop_excluded = excluded / (included + excluded)) %>%
+    mutate(prop_included = ifelse(is.nan(prop_included), 0, prop_included), 
+      prop_excluded = ifelse(is.nan(prop_excluded), 0, 
+        prop_excluded)) %>%
+    mutate(adjusted_included = included  + unknown * prop_included,
+      adjusted_excluded = excluded + unknown * prop_excluded)
+  
+  hivResults <- data_frame(year = adjustDiags$year,
+    notifications = adjustDiags$adjusted_included,
+    cumnotifications = cumsum(adjustDiags$adjusted_included),
+    all_notifications = adjustDiags$all,
+    all_cumnotifications = cumsum(adjustDiags$all))
+  
+  # Proportion in each age group--------------------------------------------
+  # For the known diagnoses calculate proportion by age bin, exposure group,
+  # cd4 count category and diagnosis type. This will be used to distribute 
+  # annual notifications for aging PLDHIV.  
+  
+  hivResultsAge <- NULL #initialize
+  if (doAge) {
+    agedDiags <- hivSet %>%
+      group_by(yeardiagnosis, agebin) %>%
+      summarise(diags = n()) %>%
+      ungroup() %>%
+      spread(agebin, diags) %>%
+      rename(year = yeardiagnosis)
+    agedDiags[is.na(agedDiags)] <- 0
+    
+    #fill missing years
+    agedDiags <- FillDataFrame(allYears, agedDiags) %>%
+      gather("agebin", "diagnoses", 2:20) %>%
+      arrange(year) %>%
+      filter(agebin != "not_reported") %>%
+      group_by(year) %>%
+      mutate(propdiags = diagnoses / sum(diagnoses)) %>%
+      mutate(propdiags = ifelse(is.nan(propdiags), 0 , propdiags)) %>%
+      ungroup()
+    
+    nyears <- tail(allYears, 1) - allYears[1] + 1
+    nages <- 18
+    
+    ageList <- c("a0_4", "a5_9","a10_14", "a15_19", "a20_24", "a25_29", 
+      "a30_34", "a35_39", "a40_44", "a45_49", "a50_54", "a55_59", 
+      "a60_64", "a65_69", "a70_74", "a75_79", "a80_84", "a85+")
+    
+    yearList <- paste0("y", as.character(allYears))
+    
+    # Adjust_included notifications by age and year and convert to matrix
+    hivResultsAge <- agedDiags %>% 
+      left_join(., hivResults, by = "year") %>% 
+      select(-diagnoses, -cumnotifications, -all_notifications,
+        -all_cumnotifications) %>%
+      group_by(year, agebin) %>%
+      mutate(diags = propdiags * notifications) %>%
+      select(-propdiags, -notifications) %>%
+      spread(year, diags) %>%
+      ungroup() %>%
+      slice(c(1,10,2:9,11:18)) %>%
+      select(-agebin) %>%
+      as.matrix()
+    
+    colnames(hivResultsAge) <- yearList
+    rownames(hivResultsAge) <- ageList
+  }
+  
+  # Return list of hiv results
+  return(list(hivResults, hivResultsAge))
+  
+}
