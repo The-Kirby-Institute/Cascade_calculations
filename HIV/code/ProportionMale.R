57bc91d390227e5ecde619b207af32217dc9cd2a
leftygray <leftygray@gmail.com>
2019-02-21 23:01:13 +1100

    Fixed up an issue with unknown not existing when doing imputed sets

--- HIV/code/ProportionMale.R
+++ HIV/code/ProportionMale.R
@@ -49,10 +49,17 @@ ProportionMale <- function(hivSet, analysisYear, doAge,
         ungroup() %>%
         spread(sex, notifications)
       hivSetGenderAge[is.na(hivSetGenderAge)] <- 0
-      hivSetGenderAge <- hivSetGenderAge %>%
-        mutate(other = female+transgender+unknown) %>%
-        rename(year = yeardiagnosis) %>%
-        select(year, agebin, male, other)
+      if ("unknown" %in% names(hivSetGenderAge)) {
+        hivSetGenderAge <- hivSetGenderAge %>%
+          mutate(other = female+transgender+unknown) %>%
+          rename(year = yeardiagnosis) %>%
+          select(year, agebin, male, other)
+      } else {
+        hivSetGenderAge <- hivSetGenderAge %>%
+          mutate(other = female+transgender) %>%
+          rename(year = yeardiagnosis) %>%
+          select(year, agebin, male, other)
+      }
       hivSetGenderAge[is.na(hivSetGenderAge)] <- 0
       
       # Fill in missing years with zeros - have to do males and others and
