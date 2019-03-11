ab349a56ea3399e68e941ea03fd003337145111e
leftygray <leftygray@gmail.com>
2019-03-11 14:10:12 +1100

    Set-up pldhiv calculation function

---
+++ HIV/code/CalculatePldhiv.R
@@ -0,0 +1,35 @@
+#' Calculate the annual number of people living with diagnosed HIV'
+#' 
+#' This function calculates the number of people diagnosed with HIV each 
+#' year for a specific set of criteria. It also produces files for running 
+#' in teh ECDC HIV Modelling Tool and produces projected estimates as 
+#' required. It is the main function call by the 1-PldhivCalculations.Rmd 
+#' script. A lot of functions need to be sourced prior to calling for this 
+#' function to run.   
+#' 
+#' @param analysisYear Year to run calculations to
+#' 
+#' @return A list containing multiple data frames
+#' 
+#' @author Richard T Gray, \email{Rgray@kirby.unsw.edu.au}
+#' 
+#' @export
+#' @import tidyverse
+#'  
+CalculatePldhiv <- function(analysisYear, saveResults, projectOutput, 
+  cascadeName, targetGender, targetAge, targetCob, targetExposure, 
+  targetAtsi, targetState, targetLocalRegion, targetGlobalRegion, 
+  excludeAborig, doRetained, doUnique, yearUnique, ecdcData, ecdcVersion, 
+  excludeOS, projectPldhiv, projectYear, projectName, projectOption, 
+  projectDecrease, hivData, allYears, hivBase, hivAdjustments, cleanNom,
+  absInterstate, ageList, yearList, hivAgeDeath) {
+  
+  
+  
+  
+  
+  
+  
+  
+  return(pldhivCalculations)
+}
